67a73b6027beb785b7a6f9e965d67deb4735fcb3
Grzegorz Cieslik <grzegorz.cieslik@hotmail.com>
2021-02-11 19:55:48 +0100

    initial commit

---
+++ src/main/java/com/grzegorzcieslik/sprtingtut/SprtingtutApplication.java
@@ -0,0 +1,13 @@
+package com.grzegorzcieslik.sprtingtut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprtingtutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprtingtutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grzegorzcieslik/sprtingtut/SprtingtutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grzegorzcieslik.sprtingtut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprtingtutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
