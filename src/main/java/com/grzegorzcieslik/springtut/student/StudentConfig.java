6de90a5f224539208ccdd7a38062f0b307994853
Grzegorz Cieslik <grzegorz.cieslik@hotmail.com>
2021-02-18 19:36:55 +0100

    added post mapping

--- src/main/java/com/grzegorzcieslik/springtut/student/StudentConfig.java
+++ src/main/java/com/grzegorzcieslik/springtut/student/StudentConfig.java
@@ -15,10 +15,10 @@ public class StudentConfig {
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
             Student mariam = new Student(
-                    "Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21
+                    "Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5)
             );
             Student alex = new Student(
-                    "Alex", "alex@gmail.com", LocalDate.of(2004, Month.JANUARY, 5), 17
+                    "Alex", "alex@gmail.com", LocalDate.of(2004, Month.JANUARY, 5)
             );
 
             repository.saveAll(
